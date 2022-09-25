b7249666805b458e5ee1bea3b4d1949f6a53029a
Branchey <70140890+CZboop@users.noreply.github.com>
2022-09-25 11:51:48 +0100

    fixed error in stats dialog

--- components/stats_dialog.py
+++ components/stats_dialog.py
@@ -15,36 +15,39 @@ class StatsDialog(qtw.QDialog):
         self.adj_count = stats[4]
         self.adv_count = stats[5]
 
-        # setting up dialog window properties
-        def window_setup(self):
-            self.setWindowTitle("Statistics")
-            width, height = 450, 400
-            self.setMinimumSize(width, height)
+        self.window_setup()
+        self.widget_setup()
 
-            pixmapi = qtw.QStyle.SP_FileDialogInfoView
-            icon = self.style().standardIcon(pixmapi)
-            self.setWindowIcon(icon)
+    # setting up dialog window properties
+    def window_setup(self):
+        self.setWindowTitle("Statistics")
+        width, height = 450, 400
+        self.setMinimumSize(width, height)
 
-        # setting up layout and widgets within it
-        def widget_setup(self):
-            # layout and static labels to just display the stats passed from mainwindow to dialog
-            self.layout = qtw.QVBoxLayout()
+        pixmapi = qtw.QStyle.SP_FileDialogInfoView
+        icon = self.style().standardIcon(pixmapi)
+        self.setWindowIcon(icon)
 
-            self.word_count_label = qtw.QLabel(f"Word Count: {self.word_count}")
-            self.layout.addWidget(self.word_count_label)
-            self.char_count_label = qtw.QLabel(f"Character Count: {self.char_count}")
-            self.layout.addWidget(self.char_count_label)
+    # setting up layout and widgets within it
+    def widget_setup(self):
+        # layout and static labels to just display the stats passed from mainwindow to dialog
+        self.layout = qtw.QVBoxLayout()
 
-            self.noun_count_label = qtw.QLabel(f"Noun Count: {self.noun_count}")
-            self.layout.addWidget(self.noun_count_label)
-            self.verb_count_label = qtw.QLabel(f"Verb Count: {self.verb_count}")
-            self.layout.addWidget(self.verb_count_label)
-            self.adj_count_label = qtw.QLabel(f"Adjective Count: {self.adj_count}")
-            self.layout.addWidget(self.adj_count_label)
-            self.adv_count_label = qtw.QLabel(f"Adverb Count: {self.adv_count}")
-            self.layout.addWidget(self.adv_count_label)
+        self.word_count_label = qtw.QLabel(f"Word Count: {self.word_count}")
+        self.layout.addWidget(self.word_count_label)
+        self.char_count_label = qtw.QLabel(f"Character Count: {self.char_count}")
+        self.layout.addWidget(self.char_count_label)
 
-            self.setLayout(self.layout)
+        self.noun_count_label = qtw.QLabel(f"Noun Count: {self.noun_count}")
+        self.layout.addWidget(self.noun_count_label)
+        self.verb_count_label = qtw.QLabel(f"Verb Count: {self.verb_count}")
+        self.layout.addWidget(self.verb_count_label)
+        self.adj_count_label = qtw.QLabel(f"Adjective Count: {self.adj_count}")
+        self.layout.addWidget(self.adj_count_label)
+        self.adv_count_label = qtw.QLabel(f"Adverb Count: {self.adv_count}")
+        self.layout.addWidget(self.adv_count_label)
+
+        self.setLayout(self.layout)
 
     def submitclose(self):
         self.accept()
